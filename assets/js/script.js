292d35ab8f93d8f4ac69ee06039b2b02f7e837df
Melanie <melanie.collins.p@gmail.com>
2024-03-07 14:59:15 -0300

    construir objeto myHero con datos dinamicos de la API

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,13 +26,39 @@ $(document).ready(function () {
 
     // consumir la API
     // url: https://www.superheroapi.com/api.php/4905856019427443/100
+
     const getHero = (heroNumberFn) => {
 
         $.ajax({
             url: "https://www.superheroapi.com/api.php/4905856019427443/" + heroNumberFn,
             method: "GET",
-            success(data) {
-                console.log(data)
+            success(hero) {
+                const aliasesArray = hero.biography.aliases;
+                const aliasesString = aliasesArray.join(', ');
+
+                // console.log("img:", hero.image.url)
+                // console.log("Nombre:", hero.name)
+                // console.log("Aliases:", aliasesString)
+                // console.log("Ocupación:", hero.work.occupation)
+                // console.log("Publicado por:", hero.biography.publisher)
+                // console.log("Primera aparición:", hero.biography['first-appearance'])
+                // console.log("Conexiones:", hero.connections['group-affiliation'])
+                // console.log("Altura:", hero.appearance.height[1])
+                // console.log("Peso:", hero.appearance.weight[1])
+
+                //construir un objeto
+                const myHero = {
+                    image: hero.image.url,
+                    name: hero.name,
+                    aliases: aliasesString,
+                    occupation: hero.work.occupation,
+                    publisher: hero.biography.publisher,
+                    firstAppereance: hero.biography['first-appearance'],
+                    connections: hero.connections['group-affiliation'],
+                    height: hero.appearance.height[1],
+                    weight: hero.appearance.weight[1]
+                }
+                console.log(myHero)
             },
             error(e) {
                 console.log(e)
