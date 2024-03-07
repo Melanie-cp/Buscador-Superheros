479b4beb867708965dd114ef0f7c21c0214233b8
Melanie <melanie.collins.p@gmail.com>
2024-03-07 13:23:46 -0300

    capturamos los elementos del DOM y procesamos el formulario con validaciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
-//eperar q cargue el dom
+//eperar que cargue el dom
 $(document).ready(function () {
+    //Capturar los elementos del DOM
     const heroForm = $("#heroForm")
     const heroNumber = $("#heroNumber")
     const heroResult = $("#heroResult")
+
+    // Procesar el formulario
+    heroForm.on("submit", function (event) {
+        event.preventDefault()
+
+        //rremover clases is-valid is-invalid
+        heroNumber.removeClass("is-valid is-invalid")
+
+        //capturar lo que escribió usuario en input
+        const heroNumberInput = +heroNumber.val()
+
+        //validaciones
+        if (heroNumberInput > 0 && heroNumberInput <= 731) {
+            heroNumber.addClass("is-valid")
+        } else {
+            heroNumber.addClass("is-invalid")
+        }
+
+    })
 })
